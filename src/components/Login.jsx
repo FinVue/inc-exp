22350f686f19875206d7613e6a57556781549c74
Ram Baarde <ramchrist20@gmail.com>
2024-05-10 18:43:29 +0800

    update sum

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,7 +6,7 @@ import { Label } from "@/components/ui/label";
 import { initializeApp } from "firebase/app";
 import { getAuth, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { getFirestore, collection, onSnapshot } from "firebase/firestore";
-import toast from 'react-hot-toast'; // Import toast from react-hot-toast
+import toast from 'react-hot-toast'; 
 
 const firebaseConfig = {
   apiKey: "AIzaSyDoRWJ-StU751uUFka1qDgNBslu9hEaunA",
@@ -18,7 +18,6 @@ const firebaseConfig = {
   measurementId: "G-YYWBWQ3JYZ"
 };
 
-// Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
 const auth = getAuth(firebaseApp);
 const db = getFirestore(firebaseApp);
@@ -40,7 +39,7 @@ const Login = () => {
       });
     });
   
-    return () => unsubscribe(); // Unsubscribe from Firestore listener on component unmount
+    return () => unsubscribe(); 
   }, [db]);
 
   const handleLogin = async (e) => {
@@ -129,7 +128,7 @@ const Login = () => {
                 width="24"
                 height="24"
                 viewBox="0 0 48 48"
-                className="mr-2" // Added margin to the right for spacing
+                className="mr-2" 
                 >
                 <path fill="#fbc02d" d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12	s5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24s8.955,20,20,20	s20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"></path>
                 <path fill="#e53935" d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039	l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"></path>
